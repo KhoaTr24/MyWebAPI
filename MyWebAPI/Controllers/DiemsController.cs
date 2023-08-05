b16a3b811d0c19baf258ec851eae50e1add8ef1f
Khoa <khoat2406@gmail.com>
2023-08-05 16:07:05 +0700

    Saved

--- MyWebAPI/Controllers/DiemsController.cs
+++ MyWebAPI/Controllers/DiemsController.cs
@@ -34,7 +34,7 @@ namespace MyWebAPI.Controllers
             var diems = await _context.Diem.FindAsync(id);
             return diems == null ? NotFound() : Ok(diems);
         }
-
+        //
         // PUT
         [HttpPut("{id}")]
         public async Task<IActionResult> PutDiem(int id, Diem diem)
